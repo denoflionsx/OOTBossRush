453d1c5a666ce7cc4e50a8fa068c01283316e99a
Denoflions <denvstheworld@gmail.com>
2021-10-11 08:49:07 -0400

    Update for Z64O Nightlies

--- src/OotBossRush/OotBossRush.ts
+++ src/OotBossRush/OotBossRush.ts
@@ -1,27 +1,14 @@
-import {IPlugin, IModLoaderAPI} from 'modloader64_api/IModLoaderAPI';
-import {IOOTCore} from 'modloader64_api/OOT/OOTAPI';
-import {InjectCore} from 'modloader64_api/CoreInjection';
+import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { SidedProxy, ProxySide } from 'modloader64_api/SidedProxy/SidedProxy';
 import { OotBossRushClient } from './OotBossRushClient';
 
-class OotBossRush implements IPlugin{
+class OotBossRush {
 
     ModLoader!: IModLoaderAPI;
     pluginName?: string | undefined;
-    @InjectCore()
-    core!: IOOTCore;
     @SidedProxy(ProxySide.CLIENT, OotBossRushClient)
     client!: OotBossRushClient;
-
-    preinit(): void {
-    }
-    init(): void {
-    }
-    postinit(): void {
-    }
-    onTick(frame?: number | undefined): void {
-    }
-
+    
 }
 
 module.exports = OotBossRush;
--- src/OotBossRush/OotBossRushClient.ts
+++ src/OotBossRush/OotBossRushClient.ts
@@ -1,15 +1,24 @@
 import { onTick, Postinit } from 'modloader64_api/PluginLifecycle';
-import { IOOTCore, OotEvents, InventoryItem, AmmoUpgrade, UpgradeCountLookup, Ocarina, Strength, ZoraScale, Hookshot, Magic, MagicQuantities } from 'modloader64_api/OOT/OOTAPI';
 import { InjectCore } from 'modloader64_api/CoreInjection';
 import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { ModLoaderAPIInject } from "modloader64_api/ModLoaderAPIInjector";
-import { EventHandler } from 'modloader64_api/EventHandler';
-import { OotOnlineAPI_EnableGhostMode } from './OotoAPI/OotoAPI';
+import { bus, EventHandler } from 'modloader64_api/EventHandler';
+import { IZ64Main } from 'Z64Lib/API/Common/IZ64Main';
+import { AmmoUpgrade, Magic, MagicQuantities, Ocarina, OotEvents, UpgradeCountLookup } from 'Z64Lib/API/Common/Z64API';
+import { InventoryItem, Strength, ZoraScale, Hookshot } from 'Z64Lib/API/OoT/OOTAPI';
+
+const enum Z64OnlineEvents {
+    GHOST_MODE = 'Z64Online:EnableGhostMode'
+}
+
+function Z64O_OOT_EnableGhostMode() {
+    bus.emit(Z64OnlineEvents.GHOST_MODE, {});
+}
 
 export class OotBossRushClient {
 
     @InjectCore()
-    core!: IOOTCore;
+    core!: IZ64Main;
     @ModLoaderAPIInject()
     ModLoader!: IModLoaderAPI;
     kits: Map<number, SceneKit> = new Map<number, SceneKit>();
@@ -18,8 +27,8 @@ export class OotBossRushClient {
 
     healPlayer() {
         if (
-            this.core.helper.isTitleScreen() ||
-            !this.core.helper.isSceneNumberValid()
+            this.core.OOT!.helper.isTitleScreen() ||
+            !this.core.OOT!.helper.isSceneNumberValid()
         ) {
             return;
         }
@@ -31,123 +40,121 @@ export class OotBossRushClient {
 
     @Postinit()
     onPostInit() {
-        OotOnlineAPI_EnableGhostMode();
+        Z64O_OOT_EnableGhostMode();
         let link = 0x0034;
         this.kits.set(link, new SceneKit(link, () => {
-            this.core.save.boots.kokiriBoots = true;
-            this.core.save.tunics.kokiriTunic = true;
-            this.core.save.swords.kokiriSword = true;
+            this.core.OOT!.save.boots.kokiriBoots = true;
+            this.core.OOT!.save.tunics.kokiriTunic = true;
+            this.core.OOT!.save.swords.kokiriSword = true;
         }));
         let gohma = 0x0011;
         this.kits.set(gohma, new SceneKit(gohma, () => {
             setInterval(() => {
-                this.core.save.questStatus.displayGoldSkulltulas = true;
-                this.core.save.questStatus.goldSkulltulas = this.core.save.questStatus.goldSkulltulas + 1;
+                this.core.OOT!.save.questStatus.displayGoldSkulltulas = true;
+                this.core.OOT!.save.questStatus.goldSkulltulas = this.core.OOT!.save.questStatus.goldSkulltulas + 1;
             }, 60 * 1000);
             // Equipment
-            this.core.save.shields.dekuShield = true;
+            this.core.OOT!.save.shields.dekuShield = true;
             // Inventory
             // Bullet Bag
-            this.core.save.inventory.bulletBag = AmmoUpgrade.BASE;
-            this.core.save.inventory.fairySlingshot = true;
-            this.core.save.inventory.dekuSeeds = UpgradeCountLookup(InventoryItem.FAIRY_SLINGSHOT, this.core.save.inventory.bulletBag);
+            this.core.OOT!.save.inventory.bulletBag = AmmoUpgrade.BASE;
+            this.core.OOT!.save.inventory.fairySlingshot = true;
+            this.core.OOT!.save.inventory.dekuSeeds = UpgradeCountLookup(InventoryItem.FAIRY_SLINGSHOT, this.core.OOT!.save.inventory.bulletBag);
             // Deku Nuts
-            this.core.save.inventory.dekuNutsCapacity = AmmoUpgrade.BASE;
-            this.core.save.inventory.dekuNutsCount = UpgradeCountLookup(InventoryItem.DEKU_NUT, this.core.save.inventory.dekuNutsCapacity);
-            this.core.save.inventory.dekuNuts = true;
+            this.core.OOT!.save.inventory.dekuNutsCapacity = AmmoUpgrade.BASE;
+            this.core.OOT!.save.inventory.dekuNutsCount = UpgradeCountLookup(InventoryItem.DEKU_NUT, this.core.OOT!.save.inventory.dekuNutsCapacity);
+            this.core.OOT!.save.inventory.dekuNuts = true;
             // Deku Sticks.
-            this.core.save.inventory.dekuSticksCapacity = AmmoUpgrade.BASE;
-            this.core.save.inventory.dekuSticksCount = UpgradeCountLookup(InventoryItem.DEKU_STICK, this.core.save.inventory.dekuSticksCapacity);
-            this.core.save.inventory.dekuSticks = true;
+            this.core.OOT!.save.inventory.dekuSticksCapacity = AmmoUpgrade.BASE;
+            this.core.OOT!.save.inventory.dekuSticksCount = UpgradeCountLookup(InventoryItem.DEKU_STICK, this.core.OOT!.save.inventory.dekuSticksCapacity);
+            this.core.OOT!.save.inventory.dekuSticks = true;
         }));
         let dodongo = 0x0012;
         this.kits.set(dodongo, new SceneKit(dodongo, () => {
-            this.core.save.inventory.ocarina = Ocarina.FAIRY_OCARINA;
-            this.core.save.questStatus.zeldasLullaby = true;
-            this.core.save.questStatus.eponasSong = true;
-            this.core.save.questStatus.sunsSong = true;
-            this.core.save.questStatus.sariasSong = true;
-            this.core.save.inventory.strength = Strength.GORON_BRACELET;
-            this.core.save.shields.hylianShield = true;
-            this.core.save.inventory.bombs = true;
-            this.core.save.inventory.bombBag = AmmoUpgrade.BASE;
-            this.core.save.inventory.bombsCount = UpgradeCountLookup(InventoryItem.BOMB, this.core.save.inventory.bombBag);
-            this.core.save.inventory.bottle_1 = InventoryItem.BOTTLED_FAIRY;
-            this.core.save.inventory.childTradeItem = InventoryItem.ZELDAS_LETTER;
+            this.core.OOT!.save.inventory.ocarina = Ocarina.FAIRY_OCARINA;
+            this.core.OOT!.save.questStatus.zeldasLullaby = true;
+            this.core.OOT!.save.questStatus.eponasSong = true;
+            this.core.OOT!.save.questStatus.sunsSong = true;
+            this.core.OOT!.save.questStatus.sariasSong = true;
+            this.core.OOT!.save.inventory.strength = Strength.GORON_BRACELET;
+            this.core.OOT!.save.shields.hylianShield = true;
+            this.core.OOT!.save.inventory.bombs = true;
+            this.core.OOT!.save.inventory.bombBag = AmmoUpgrade.BASE;
+            this.core.OOT!.save.inventory.bombsCount = UpgradeCountLookup(InventoryItem.BOMB, this.core.OOT!.save.inventory.bombBag);
+            this.core.OOT!.save.inventory.bottle_1 = InventoryItem.BOTTLED_FAIRY;
+            this.core.OOT!.save.inventory.childTradeItem = InventoryItem.ZELDAS_LETTER;
         }));
         let barinade = 0x0013;
         this.kits.set(barinade, new SceneKit(barinade, () => {
-            this.core.save.inventory.boomerang = true;
-            this.core.save.inventory.swimming = ZoraScale.SILVER;
-            this.core.save.inventory.bottle_2 = InventoryItem.BOTTLED_FAIRY;
-            this.core.save.magic_meter_size = Magic.NORMAL;
-            this.core.save.magic_current = MagicQuantities.NORMAL;
+            this.core.OOT!.save.inventory.boomerang = true;
+            this.core.OOT!.save.inventory.swimming = ZoraScale.SILVER;
+            this.core.OOT!.save.inventory.bottle_2 = InventoryItem.BOTTLED_FAIRY;
+            this.core.OOT!.save.magic_meter_size = Magic.NORMAL;
+            this.core.OOT!.save.magic_current = MagicQuantities.NORMAL;
         }));
         let tot = 0x0043;
         this.kits.set(tot, new SceneKit(tot, () => {
-            if (this.core.save.questStatus.goldSkulltulas <= 5) {
-                this.core.save.swords.biggoronSword = true;
+            if (this.core.OOT!.save.questStatus.goldSkulltulas <= 5) {
+                this.core.OOT!.save.swords.biggoronSword = true;
             }
-            this.core.save.questStatus.songOfTime = true;
-            this.core.save.magic_meter_size = Magic.NORMAL;
-            this.core.save.magic_current = MagicQuantities.NORMAL;
-            this.core.save.inventory.bombsCount = UpgradeCountLookup(InventoryItem.BOMB, this.core.save.inventory.bombBag);
+            this.core.OOT!.save.questStatus.songOfTime = true;
+            this.core.OOT!.save.magic_meter_size = Magic.NORMAL;
+            this.core.OOT!.save.magic_current = MagicQuantities.NORMAL;
+            this.core.OOT!.save.inventory.bombsCount = UpgradeCountLookup(InventoryItem.BOMB, this.core.OOT!.save.inventory.bombBag);
         }));
         let phantom = 0x0014;
         this.kits.set(phantom, new SceneKit(phantom, () => {
-            this.core.save.swords.masterSword = true;
-            this.core.save.inventory.adultTradeItem = InventoryItem.POCKET_EGG;
-            this.core.save.inventory.bottle_3 = InventoryItem.BOTTLED_FAIRY;
-            this.core.save.inventory.fairyBow = true;
-            this.core.save.inventory.quiver = AmmoUpgrade.BASE;
-            this.core.save.inventory.arrows = UpgradeCountLookup(InventoryItem.FAIRY_BOW, this.core.save.inventory.quiver);
-            this.core.save.inventory.hookshot = Hookshot.HOOKSHOT;
-            this.core.save.questStatus.minuetOfForest = true;
-            this.core.save.questStatus.songOfStorms = true;
+            this.core.OOT!.save.swords.masterSword = true;
+            this.core.OOT!.save.inventory.adultTradeItem = InventoryItem.POCKET_EGG;
+            this.core.OOT!.save.inventory.bottle_3 = InventoryItem.BOTTLED_FAIRY;
+            this.core.OOT!.save.inventory.fairyBow = true;
+            this.core.OOT!.save.inventory.quiver = AmmoUpgrade.BASE;
+            this.core.OOT!.save.inventory.arrows = UpgradeCountLookup(InventoryItem.FAIRY_BOW, this.core.OOT!.save.inventory.quiver);
+            this.core.OOT!.save.inventory.hookshot = Hookshot.HOOKSHOT;
+            this.core.OOT!.save.questStatus.minuetOfForest = true;
+            this.core.OOT!.save.questStatus.songOfStorms = true;
         }));
         let volvagia = 0x0015;
         this.kits.set(volvagia, new SceneKit(phantom, () => {
             if (!this.volvagia_fix) {
-                this.core.commandBuffer.runWarp(0x0305, 0x0, () => {
-                    this.healPlayer();
-                });
+                this.core.OOT!.commandBuffer.runWarp(0x0305, 0x0);
                 this.volvagia_fix = true;
                 return;
             }
-            this.core.save.inventory.megatonHammer = true;
-            this.core.save.questStatus.boleroOfFire = true;
-            this.core.save.tunics.goronTunic = true;
-            this.core.save.inventory.bombchus = true;
+            this.core.OOT!.save.inventory.megatonHammer = true;
+            this.core.OOT!.save.questStatus.boleroOfFire = true;
+            this.core.OOT!.save.tunics.goronTunic = true;
+            this.core.OOT!.save.inventory.bombchus = true;
             setTimeout(() => {
-                this.core.save.inventory.bombchuCount = 20;
+                this.core.OOT!.save.inventory.bombchuCount = 20;
             }, 3 * 1000);
-            this.core.save.inventory.bottle_4 = InventoryItem.BOTTLED_FAIRY;
+            this.core.OOT!.save.inventory.bottle_4 = InventoryItem.BOTTLED_FAIRY;
         }));
         let morpha = 0x0016;
         this.kits.set(morpha, new SceneKit(morpha, () => {
-            this.core.save.inventory.hookshot = Hookshot.LONGSHOT;
-            this.core.save.boots.ironBoots = true;
-            this.core.save.questStatus.serenadeOfWater = true;
-            this.core.save.tunics.zoraTunic = true;
+            this.core.OOT!.save.inventory.hookshot = Hookshot.LONGSHOT;
+            this.core.OOT!.save.boots.ironBoots = true;
+            this.core.OOT!.save.questStatus.serenadeOfWater = true;
+            this.core.OOT!.save.tunics.zoraTunic = true;
         }));
         let bongo = 0x0018;
         this.kits.set(bongo, new SceneKit(bongo, () => {
-            this.core.save.questStatus.nocturneOfShadow = true;
-            this.core.save.boots.hoverBoots = true;
-            this.core.save.inventory.fireArrows = true;
-            this.core.save.inventory.iceArrows = true;
-            this.core.save.inventory.lensOfTruth = true;
+            this.core.OOT!.save.questStatus.nocturneOfShadow = true;
+            this.core.OOT!.save.boots.hoverBoots = true;
+            this.core.OOT!.save.inventory.fireArrows = true;
+            this.core.OOT!.save.inventory.iceArrows = true;
+            this.core.OOT!.save.inventory.lensOfTruth = true;
         }));
         let twinrova = 0x0017;
         this.kits.set(twinrova, new SceneKit(twinrova, () => {
-            this.core.save.questStatus.requiemOfSpirit = true;
-            this.core.save.shields.mirrorShield = true;
-            this.core.save.inventory.strength = Strength.SILVER_GAUNTLETS;
+            this.core.OOT!.save.questStatus.requiemOfSpirit = true;
+            this.core.OOT!.save.shields.mirrorShield = true;
+            this.core.OOT!.save.inventory.strength = Strength.SILVER_GAUNTLETS;
         }));
         let ganondorf = 0x0019;
         this.kits.set(ganondorf, new SceneKit(ganondorf, () => {
-            this.core.save.inventory.strength = Strength.GOLDEN_GAUNTLETS;
-            this.core.save.inventory.lightArrows = true;
+            this.core.OOT!.save.inventory.strength = Strength.GOLDEN_GAUNTLETS;
+            this.core.OOT!.save.inventory.lightArrows = true;
             this.disableWarps = true;
         }));
     }
@@ -184,17 +191,17 @@ export class OotBossRushClient {
         if (!this.disableWarps) {
             // Morpha Blue Warp -> Bongo
             if (scene === 0x0057) {
-                this.core.commandBuffer.runWarp(0x0413, 0x0, () => { });
+                this.core.OOT!.commandBuffer.runWarp(0x0413, 0x0);
                 return;
             }
             // Bongo Blue Warp -> Twinrova
             if (scene === 0x0053) {
-                this.core.commandBuffer.runWarp(0x008D, 0x0, () => { });
+                this.core.OOT!.commandBuffer.runWarp(0x008D, 0x0);
                 return;
             }
             // Twinrova Blue Warp -> Ganondorf
             if (scene === 0x005C) {
-                this.core.commandBuffer.runWarp(0x041F, 0x0, () => { });
+                this.core.OOT!.commandBuffer.runWarp(0x041F, 0x0);
                 return;
             }
         }
